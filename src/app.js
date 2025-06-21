29b61f72764a31d1f00034a8e29425988b8e0ffd
ramsesfg <zad.yadiel12@gmail.com>
2025-06-21 01:08:17 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-import "bootstrap";
-import "./style.css";
+let pronombres = ['el', 'tu' , 'yo','nosotros', 'ellos'];
+let adjetivos = ['grande', 'loco', 'triste', 'feliz', 'cansado'];
+let sustantivos = ['libro', 'ciudad', 'justicia', 'libertad'];
+let dominios= ['.com', '.cl', '.net', '.ve', '.ec'];
 
+const combinaciones = () =>{
+  for (let index = 0; index < pronombres.length; index++) {
+    const elementos = pronombres[index];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    for (let indexAdjetivos = 0; indexAdjetivos < adjetivos.length; indexAdjetivos++){
+      const eleAdjetivos = adjetivos[indexAdjetivos];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+      for (let indexSustantivos = 0; indexSustantivos < sustantivos.length; indexSustantivos++){
+        const susSustantivos = sustantivos[indexSustantivos];
+
+        for (let indexDominios = 0; indexDominios < dominios.length; indexDominios++){
+          const domDominios = dominios[indexDominios];
+
+          let resultado = `${elementos}${eleAdjetivos}${susSustantivos}${domDominios}`;
+          console.log(resultado);
+        
+        } 
+      }
+      
+    }
+    
+  }
+  
 };
+combinaciones();
